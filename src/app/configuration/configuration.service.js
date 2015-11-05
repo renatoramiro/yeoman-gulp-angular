e1b85802358ae3236b86d90ca68882e053783237
Renato Ramiro <renato.ramiro@embedded.ufcg.edu.br>
2015-11-05 14:37:06 -0300

    Fixing bug

--- src/app/configuration/configuration.service.js
+++ src/app/configuration/configuration.service.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
-  angular.module('medicos2').service('Configuration', function () {
+  angular.module('medicos2').service('Configuration', function ($location) {
 
-    if (window.location.host.indexof("heroku") > -1) {
+    if ($location.host().indexOf("heroku") > -1) {
       return this.API = 'https://fast-plains-6359.herokuapp.com';
     } else {
       return this.API = 'http://localhost:4000';
